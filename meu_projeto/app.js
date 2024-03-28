53d772e127ef6a4b430b4e8623bc3306bee43d14
Tarsila Moraes <tarsilamoraes87@icloud.com>
2024-03-28 19:50:21 -0300

    Inclusão de rotas

--- meu_projeto/app.js
+++ meu_projeto/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+const PORT = 3000;
+
+app.get('/sobre', (req, res) => {
+  res.send('Sobre: Este é um aplicativo criado com Express.js.');
+});
+
+app.get('/contato', (req, res) => {
+  res.send('Contato: 0800-000-00');
+});
 
 app.get('/', (req, res) => {
   res.send('Olá, mundo!');
 });
 
-const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor Express escutando na porta ${PORT}`);
+  console.log(`Servidor Express está ouvindo na porta ${PORT}`);
 });
